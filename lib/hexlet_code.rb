b5f494d9983602b685895473ae37c74f35b932ff
ivanz <xvanx84@gmail.com>
2024-02-15 18:12:23 +0100

    add form creation

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -6,4 +6,8 @@ module HexletCode
   autoload(:Tag, 'hexlet_code/tag.rb')
 
   class Error < StandardError; end
+
+  def self.form_for(_model, url: '#')
+    Tag.build('form', action: url, method: 'post')
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -6,4 +6,14 @@ class TestHexletCode < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
+
+  def test_form_for_user
+    user = HexletCode.form_for :user
+    assert_equal user, '<form action="#" method="post"></form>'
+  end
+
+  def test_form_for_url
+    user = HexletCode.form_for :user, url: '/users'
+    assert_equal user, '<form action="/users" method="post"></form>'
+  end
 end
